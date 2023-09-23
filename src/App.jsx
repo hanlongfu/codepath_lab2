34f43aa19049e6d0049a2cc21cd813c4b02a7e12
Hanlong Fu <hanlongfu@Hanlongs-Laptop.attlocal.net>
2023-09-23 14:54:31 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import './App.css';
+import {useState} from 'react';
+
+
+const App = () => {
+
+const [count, setCount] = useState(0);
+
+const [multiplier, setMultiplier] = useState(1);
+
+const updateCount = () => setCount(count + multiplier);
+
+const buyDoubleStuffed = () => {
+  if (count >= 10) {
+    setMultiplier(multiplier * 2);
+  }
+}
+
+const buyPartyPack = () => {
+  if(count >= 100) {
+    setMultiplier(multiplier * 5);
+  }
+}
+
+const buyFullFeast = () =>{
+  if(count >= 1000) {
+    setMultiplier(multiplier * 10);
+  }
+}
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count: {count} </h2>
+        <img className="samosa" src="/assets/samosa.png" alt="" onClick= {updateCount} />
+      </div>
+      <div className="container">
+        <div className="upgrade">
+          <h3>Double Stuffed</h3>
+          <p>2x per click</p>
+          <button onClick={buyDoubleStuffed}>10 Samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Party Pack</h3>
+          <p>5x per click</p>
+          <button onClick={buyPartyPack}>100 Samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Full Feast</h3>
+          <p>10x per click</p>
+          <button onClick={buyFullFeast}>1000 Samosas</button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
